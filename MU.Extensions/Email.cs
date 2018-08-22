8716edb4bf1e6e575ff7af54aeb882f558e0852a
dingxuanwei <dingdings02@163.com>
2018-08-22 22:05:42 +0800

    添加了发送邮件功能

---
+++ MU.Extensions/Email.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace MU.Extensions
+{
+    public class Email
+    {
+        public static void Send(string emailAddress, string html)
+        {
+            var frommail= "dingxuanwei@shtmu.com";
+            var password = "38158738a";
+             MailMessage mail = new MailMessage();
+            mail.Subject = "测试邮件";
+            mail.From = new MailAddress(frommail, "上海秒优");
+            mail.To.Add(new MailAddress(emailAddress, "noreply"));
+            mail.Body = html;
+            mail.BodyEncoding = Encoding.UTF8;
+            mail.IsBodyHtml = true;
+            mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnSuccess;
+            SmtpClient client = new SmtpClient();
+            client.Host = "smtp.ym.163.com";
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential(frommail, password);
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.Send(mail);
+        }
+    }
+}
--- MUERP/Controllers/HomeController.cs
+++ MUERP/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using MUSystem.Core;
+﻿using MU.Extensions;
 using MUSystem.DBModel;
 using System;
 using System.Collections.Generic;
@@ -12,7 +12,9 @@ namespace MUERP.Controllers
     {
         public ActionResult Index( )
         {
-            return View( );
+            Email.Send("dingdings02@163.com", "<h1>Hello dingdings02@163.com</h1>");
+
+            return View();
         }
 
         public ActionResult About( )
@@ -27,47 +29,6 @@ namespace MUERP.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View( );
-        }
-
-        
-        public dynamic GetOrderList(RequestWrapper query)
-        {
-            query.LoadSettingXmlString(@"
-<settings defaultOrderBy='a.BillDate desc'>
-    <select>
-        A.*,B.ShortName as CustomerName,s.OrderQty,D.Text AS BrandText
-    </select>
-    <from>
-        oms_order A
-        left join bas_customer     B  on     B.CustomerCode = A.CustomerCode
-        left join (select BillNo ,sum(SizeQty) as OrderQty from oms_orderSizeList group by BillNo) S on A.BillNo =S.BillNo 
-        left join bas_customerBrand D on D.Value = A.Brand 
-        inner join (select distinct PatternBillNo from otk_ProofingReady) otk on A.BillNo=otk.PatternBillNo
-    </from>
-    <where defaultForAll='true' defaultCp='equal' defaultIgnoreEmpty='true' >
-        <field name='A.CustomerCode'        cp='equal'          ></field>
-        <field name='A.CustOrder'           cp='equal'          ></field>
-        <field name='A.ModelName'           cp='like'           ></field>
-        <field name='A.Salesman'            cp='like'           ></field>
-        <field name='A.StyleNo'             cp='like'           ></field>
-        <field name='A.StyleName'           cp='like'           ></field>
-        <field name='A.SeasonName'          cp='equal'          ></field>
-        <field name='A.BillDate'            cp='daterange'      ></field>
-        <field name='A.OrderStateCode'      cp='equal'          ></field>
-        <field name='A.Brand'               cp='like'           ></field>
-        <field name='A.Description'         cp='like'           ></field>
-    </where>
-</settings>");
-            var masterService = new oms_orderService();
-            var pQuery = query.ToParamQuery();
-            var result = masterService.GetDynamicListWithPaging(pQuery);
-            return result;
-        }
-
-        public ActionResult Test1()
-        {
-            var list = GetOrderList(new RequestWrapper());
-            return Json(list, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Test2()
