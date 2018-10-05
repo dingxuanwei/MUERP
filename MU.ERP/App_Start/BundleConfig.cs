f6e3516b9bb194e95c368ed0267d2b905293712e
dingxuanwei <dingdings02@163.com>
2018-10-05 10:33:30 +0800

    引入element-UI

--- MU.ERP/App_Start/BundleConfig.cs
+++ MU.ERP/App_Start/BundleConfig.cs
@@ -7,30 +7,19 @@ namespace MU.ERP
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            ResetIgnorePatterns(bundles.IgnoreList);
-
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/vue").Include(
+                        "~/Scripts/vue.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/easyui").Include(
-                        "~/Content/easyui/jquery.easyui.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/element").Include(
+                        "~/Content/element-ui/lib/index.js"));
 
-            bundles.Add(new StyleBundle("~/Content/easyuicss").Include(
-                      "~/Content/easyui/themes/default/easyui.css",
-                      "~/Content/easyui/themes/color.css"));
-        }
-
-        public static void ResetIgnorePatterns(IgnoreList ignoreList)
-        {
-            ignoreList.Clear();
-            ignoreList.Ignore("*.intellisense.js");
-            ignoreList.Ignore("*-vsdoc.js");
-            ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
-            //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
-            ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
+            bundles.Add(new StyleBundle("~/Content/elementui").Include(
+                      "~/Content/element-ui/lib/theme-chalk/index.css",
+                      "~/Content/fontawesome/css/animation.css",
+                      "~/Content/fontawesome/css/fontello.css"));
         }
     }
 }
