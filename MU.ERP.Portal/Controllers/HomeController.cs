7d582accfeaf9a1a4fe5da284c35c1a5f0a2b7df
dingxuanwei <dingxuanwei@shtmu.com>
2018-10-23 09:48:23 +0800

    继续制作全屏布局，导航菜单的联动

--- MU.ERP.Portal/Controllers/HomeController.cs
+++ MU.ERP.Portal/Controllers/HomeController.cs
@@ -15,15 +15,6 @@ namespace MU.ERP.Portal.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
